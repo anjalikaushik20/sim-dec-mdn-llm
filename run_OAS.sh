6fcd239cd854cea8656ac7daace4f74fd67b3d18
anjali kaushik <akaush39@asu.edu>
2025-10-24 14:52:16 -0700

    Updating requirements

---
+++ run_OAS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A grp_yanjiefu
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 0-01:00:00
+#SBATCH -p htc
+#SBATCH -q public
+#SBATCH --mem=16G
+#SBATCH --gpus-per-node=1
+#SBATCH -o output/sim_dec_mdn_llm.%j.out
+#SBATCH -e output/sim_dec_mdn_llm.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pkerhalk@asu.edu
+
+#SBATCH --export=NONE
+
+module load mamba/latest
+source activate simenv
+python3 main/cb_main.py --use_gpu 1 --dataset OAS --epochs 200 --train_mode 1 --wandb 1 --save 1
